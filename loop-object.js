a6b8aca11c1a807d8b190fbc6c4e965ed3e8dd61
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-04-25 20:51:27 +0600

    loop done

--- loop-object.js
+++ loop-object.js
@@ -11,9 +11,21 @@ console.log(keys)
 // 3. bottle[variable];//to use this we have to put key into a variable
 
 //as we got the keys into an array we can use 'for of' for that array
-
+//[N.B: this is not the best practice,we will not use it]
 for (const key of keys) {
     // console.log(key);//to get the property
     // console.log(bottle[key]);//to get the property value of bottle object
-    console.log(key, bottle[key]);//to show key and value paralal
+    // console.log(key, bottle[key]);//to show key and value paralal
+}
+
+//for in loop,we will use it mostly
+for (const key1 in bottle) {
+    // console.log(key1);
+    // console.log(bottle[key1]);
+    // console.log(key1, bottle[key1]);
+}
+
+//advanced
+for (const [key, value] of Object.entries(bottle)) {
+    console.log(key, value);
 }
