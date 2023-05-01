5bbf82e65ecedaa25942549b47b018b8d973d6f9
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-05-01 19:47:05 +0600

    function parameter finding done

--- arguments.js
+++ arguments.js
@@ -1,6 +1,13 @@
 function sum(a, b, c) {
     //showing arguments in console[N.B: arguments can be access pnly inside the function]
-    console.log(arguments)
+    //arguments is array like object
+    //we can use for loop in array like object but we cannot push/pop in array like object
+    // console.log(arguments)
+
+    //convert array like object as array by puting it in a variable
+    //... means keep all the arguments in there
+    const args = [...arguments];
+    console.log(args);
     const total = a + b + c;
     return total;
 }
@@ -8,3 +15,6 @@ function sum(a, b, c) {
 //the number that send to function at calling function time known as arguments we can access this by just arguments console.
 const result = sum(2, 3, 4, 5, 6, 6);
 console.log(result);
+
+// how we can get the parameter of a function
+console.log(sum.length)
